151fa235f429e531b7526bf8aff91fe444b5ee5b
Stef <67269037+Stef-C@users.noreply.github.com>
2024-06-20 18:55:14 +0800

    Set the default route to display products

--- routes/web.php
+++ routes/web.php
@@ -1,4 +1,4 @@
-<?php
+<!-- < ?php
 
 use Illuminate\Support\Facades\Route;
 
@@ -8,4 +8,15 @@ use Illuminate\Support\Facades\Route;
 
 use App\Http\Controllers\ProductController;
 
+// Route::get('/products', [ProductController::class, 'index']);
+
+// Route::get('/', [ProductController::class, 'index']);
+// Route::get('/products', [ProductController::class, 'index']);
+
+<?php
+
+use App\Http\Controllers\ProductController;
+use Illuminate\Support\Facades\Route;
+
+Route::get('/', [ProductController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
