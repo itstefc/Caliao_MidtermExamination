cc4161918edb71487586cec44f350eadb95aac63
Stef <67269037+Stef-C@users.noreply.github.com>
2024-06-20 19:08:02 +0800

    Finalized the display of products

--- routes/web.php
+++ routes/web.php
@@ -1,22 +1,11 @@
-<!-- < ?php
+<?php
 
+use App\Http\Controllers\ProductController;
 use Illuminate\Support\Facades\Route;
 
 // Route::get('/', function () {
 //     return view('welcome');
 // });
-
-use App\Http\Controllers\ProductController;
-
-// Route::get('/products', [ProductController::class, 'index']);
-
-// Route::get('/', [ProductController::class, 'index']);
-// Route::get('/products', [ProductController::class, 'index']);
-
-<?php
-
-use App\Http\Controllers\ProductController;
-use Illuminate\Support\Facades\Route;
 
 Route::get('/', [ProductController::class, 'index']);
 Route::get('/products', [ProductController::class, 'index']);
